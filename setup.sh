635c9c130114256ed02e52799c215c8ae9d79d55
Andrew Maudsley <andrew@andrewmaudsley.com>
2019-03-21 01:04:11 +0000

    Change order of functions in setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/bash
 
-set_default_shell() {
-  shell_name=$1
-  if chsh -s /bin/$shell_name
-  then
-    echo "Changed default shell to $shell_name"
-  else
-    echo "Unable to change default shell to $shell_name"
-  fi
-}
-
 install_homebrew() {
   if [ -x "$(command -v brew)" ]
   then
@@ -32,15 +22,15 @@ install_homebrew_packages() {
   fi
 }
 
-install_oh_my_zsh() {
-  # Install manually to prevent addition of default .zshrc
-  echo "Installing Oh-My-Zsh"
-  if git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  then
-    echo "Installed Oh-My-Zsh"
-  else
-    echo "Unable to install Oh-My-Zsh"
-  fi
+install_nvm_and_node() {
+  echo "Installing nvm"
+  export NVM_DIR="$HOME/.nvm" && (
+    git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  ) && \. "$NVM_DIR/nvm.sh"
+  echo "Installing latest Node.js release"
+  nvm install node
 }
 
 copy_examples() {
@@ -136,27 +126,37 @@ install_iterm_profile() {
   fi
 }
 
-install_nvm_and_node() {
-  echo "Installing nvm"
-  export NVM_DIR="$HOME/.nvm" && (
-    git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-    cd "$NVM_DIR"
-    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-  ) && \. "$NVM_DIR/nvm.sh"
-  echo "Installing latest Node.js release"
-  nvm install node
+install_oh_my_zsh() {
+  # Install manually to prevent addition of default .zshrc
+  echo "Installing Oh-My-Zsh"
+  if git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  then
+    echo "Installed Oh-My-Zsh"
+  else
+    echo "Unable to install Oh-My-Zsh"
+  fi
+}
+
+set_default_shell() {
+  shell_name=$1
+  if chsh -s /bin/$shell_name
+  then
+    echo "Changed default shell to $shell_name"
+  else
+    echo "Unable to change default shell to $shell_name"
+  fi
 }
 
 echo "Setup started"
-set_default_shell "zsh"
 install_homebrew
 install_homebrew_packages
-install_oh_my_zsh
+install_nvm_and_node
 copy_examples
 install_dotfiles
 install_vundle_and_plugins
 setup_font
 install_iterm_profile
-install_nvm_and_node
+install_oh_my_zsh
+set_default_shell "zsh"
 echo "Setup finished"
 echo "Please exit and start a new session for all changes to take effect"
